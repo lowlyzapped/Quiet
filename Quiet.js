bc2a2c09fb2edeb228bd5bf0615eaf279c695a01
TheV0rtex <etienne.dmenard@gmail.com>
2018-07-07 21:18:40 -0400

    More spaces.

--- Quiet.js
+++ Quiet.js
@@ -90,7 +90,7 @@ client.on('guildMemberAdd', member => { // when a member joins the server
                       .setColor(config.embedColor)
                       .setAuthor(member.guild.name+" Rules", member.guild.iconURL)
                       .setDescription(rulesText)
-                      .setFooter("Powered by "+client.user.username+"™")
+                      .setFooter("Powered by "+ client.user.username +"™")
 
                   member.send({embed}).catch(console.error);
               });
@@ -192,7 +192,7 @@ if (command === "rules") {
         .setColor(config.embedColor)
         .setAuthor(message.guild.name+" Rules", message.guild.iconURL)
         .setDescription(rulesText)
-        .setFooter("Powered by "+client.user.username+"™")
+        .setFooter("Powered by "+ client.user.username +"™")
 
     message.reply("rules have been sent.")
         .then(m => m.delete(5000));
@@ -246,7 +246,7 @@ if (command === 'servericon') {
         .setTitle(message.guild.name+"'s icon!")
         .setDescription("[Direct Link]("+message.guild.iconURL+")")
         .setImage(message.guild.iconURL)
-        .setFooter("Powered by "+client.user.username+"™")
+        .setFooter("Powered by "+ client.user.username +"™")
 
     message.channel.send({embed}).catch(console.error);
 }
@@ -262,7 +262,7 @@ if (command === 'link') {
         var embed = new Discord.RichEmbed()
             .setColor(config.embedColor)
             .setTitle("All Links")
-            .setFooter("Powered by "+client.user.username+"™")
+            .setFooter("Powered by "+ client.user.username +"™")
 
         var text = "";
         for (var i = 0; i < links.length; i++) {
@@ -291,7 +291,7 @@ if (command === 'link') {
         .setAuthor("Link: "+ links[x].name.toLowerCase())
         .setTitle(links[x].link)
         .setDescription(links[x].description)
-        .setFooter("Powered by "+client.user.username+"™")
+        .setFooter("Powered by "+ client.user.username +"™")
 
         message.channel.send({embed}).catch(console.error);
 }
@@ -664,7 +664,7 @@ if (command === 'serverinfo') {
         .addField("Verification Level:", getVerification(guild))
         .addField("Member Count:", guild.memberCount)
         .addField("Channels:", getChannels(guild))
-        .setFooter("Powered by "+client.user.username+"™")
+        .setFooter("Powered by "+ client.user.username +"™")
 
     message.channel.send({embed}).catch(console.error);
 
