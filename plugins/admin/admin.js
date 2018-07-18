3199caded4124ae680af82013cb0a5b1abede08d
TheV0rtex <etienne.dmenard@gmail.com>
2018-07-18 01:25:18 -0400

    Moved poll commands to a separate plugin in prep to new "apoll" command.

--- plugins/admin/admin.js
+++ plugins/admin/admin.js
@@ -8,8 +8,6 @@ exports.commands = [
     "purge",
     "mute",
     "unmute",
-    "poll",
-    "epoll",
     "say"
 ]
 
@@ -203,89 +201,6 @@ exports["unmute"] = {
 
             logChannel.send({embed}).catch(console.error);
         });
-    }
-}
-
-exports["poll"] = {
-    usage: "Creates a yes/no poll.",
-    needsAuth: true,
-    process: function(message, args, config) {
-        message.delete(0);
-
-        var str = message.content.slice(config.prefix.length + 4).trim();
-        var arg = str.split("|");
-        var title = arg[0];
-        var desc = arg[1];
-
-        if (arg[1] == undefined) {
-            message.reply("there are missing arguments. Please execute `"+ config.prefix +"poll <title> | <description>`.").then(m => m.delete(5000));
-            return;
-        }
-
-        var embed = new Discord.RichEmbed()
-            .setColor(config.embedColor)
-            .setTitle(title)
-            .setDescription(desc)
-            .setTimestamp()
-
-        message.channel.send({embed})
-        .then(msg => {
-              msg.react("👍");
-              msg.react("👎");
-        }).catch(console.error);
-    }
-}
-
-exports["epoll"] = {
-    usage: "Creates a poll with 2 to 5 possible choices.",
-    needsAuth: true,
-    process: function(message, args, config) {
-        message.delete(0);
-
-        var str = message.content.slice(config.prefix.length + 5).trim();
-        var arg = str.split("|");
-
-        if (arg[1] == undefined || arg[2] == undefined || arg[3] == undefined) {
-            message.reply("there are missing arguments. Please excute `"+ config.prefix +"epoll <title> | <description> | <choice 1> | <choice 2>`")
-            .then(m => m.delete(5000))
-            .catch(console.error);
-            return;
-        }
-
-        var embed = new Discord.RichEmbed()
-            .setColor(config.embedColor)
-            .setTitle(arg[0])
-            .setDescription(arg[1]+"\n\n"+
-                            ":regional_indicator_a: "+arg[2]+"\n"+
-                            ":regional_indicator_b: "+arg[3])
-            .setTimestamp()
-
-            if (arg[4] != undefined) embed.setDescription(arg[1]+"\n\n"+
-                                                          ":regional_indicator_a: "+arg[2]+"\n"+
-                                                          ":regional_indicator_b: "+arg[3]+"\n"+
-                                                          ":regional_indicator_b: "+arg[4]);
-
-            if (arg[5] != undefined) embed.setDescription(arg[1]+"\n\n"+
-                                                          ":regional_indicator_a: "+arg[2]+"\n"+
-                                                          ":regional_indicator_b: "+arg[3]+"\n"+
-                                                          ":regional_indicator_b: "+arg[4]+"\n"+
-                                                          ":regional_indicator_b: "+arg[5]);
-
-            if (arg[6] != undefined) embed.setDescription(arg[1]+"\n\n"+
-                                                          ":regional_indicator_a: "+arg[2]+"\n"+
-                                                          ":regional_indicator_b: "+arg[3]+"\n"+
-                                                          ":regional_indicator_b: "+arg[4]+"\n"+
-                                                          ":regional_indicator_b: "+arg[5]+"\n"+
-                                                          ":regional_indicator_b: "+arg[6]);
-
-            message.channel.send({embed})
-            .then(msg => {
-                msg.react("🇦");
-                msg.react("🇧");
-                if (arg[4] != undefined) msg.react("🇨");
-                if (arg[5] != undefined) msg.react("🇩");
-                if (arg[6] != undefined) msg.react("🇪");
-            }).catch(console.error);
     }
 }
 
