d038d67e7fac29116b2a4dbfc532275759066dea
TheV0rtex <etienne.dmenard@gmail.com>
2018-08-02 14:25:34 -0400

    Help command now introduced. Not much left before release. Yay!

--- Core.js
+++ Core.js
@@ -1,7 +1,10 @@
-var Discord = require('discord.js');
+var Discord = require('discord.js'),
+    fs = require('fs'),
+    path = require('path');
 
 var config = require('./config.json');
 var package = require('./package.json');
+
 var plugins = [
     'admin/admin.js',
     'bot/bot.js',
@@ -30,7 +33,7 @@ client.on('ready', () => {
 
         for (var j = 0; j < plugin.commands.length; j++) {
             commands.push(plugin.commands[j]);
-            commandObjs.push(plugin[plugin.commands[j]])
+            commandObjs.push(plugin[plugin.commands[j]]);
         }
     }
 
@@ -57,7 +60,7 @@ client.on('message', async message => {
         if (commands[i].toLowerCase() == command.toLowerCase()) {
             var cmd = commandObjs[i];
             if (cmd.needsAuth && !authorised) {
-                message.reply("you do not have the permissions needed to use this command.");
+                message.reply("you do not have the permissions needed to use this command.").then(m => m.delete(5000));
                 return;
             }
             cmd.process(message, args, config);
@@ -75,11 +78,100 @@ client.on('message', async message => {
             process.exit(1); // Stop the bot, for real.
         }
 
-        if (command === 'version') {
+        if (command === 'version') { // $version
             message.delete(0);
-
             message.channel.send("I am currently on version **"+ package.version +"**.").catch(console.error);
         }
+    }
+
+    function getDirectories(srcPath) {
+        return fs.readdirSync(srcPath)
+            .filter(f => {
+                return fs.statSync(path.join(srcPath, f))
+                    .isDirectory();
+            });
+    }
+
+    if (command === 'help') { // $help
+        message.delete(5000);
+
+        var pluginDirectory = 'plugins/';
+        var pluginFolders = null;
+
+        var result = "";
+        var member = message.member;
+        var pluginOrder = [];
+
+        var canOrder = fs.existsSync('./pluginorder.json'); // THIS WORKS
+        if (canOrder) {
+            pluginOrder = require('./pluginorder.json');
+
+            pluginOrder.sort(function(a, b) {
+                return a.sortOrder - b.sortOrder;
+            });
+
+        } else {
+            console.log("The file \"pluginorder.json\" does not exist. This file is required for custom ordering of the help command.");
+            pluginOrder = plugins.map(pd => pd.name);
+        }
+
+        var pluginsPath = path.join(__dirname, pluginDirectory); // THIS WORKS
+        if (!fs.existsSync(pluginsPath)) {
+            console.error("No plugins directory available.");
+        } else {
+            pluginFolders = getDirectories(pluginsPath);
+        }
+
+        for (var p = 0; p < plugins.length; p++) { // It's not loading...
+            var pluginData = plugins.find(pd => pd.split("/")[0] == pluginOrder[p].name);
+            if (!pluginData) continue;
+
+            var plugin = require('./plugins/' + pluginData);
+            var pluginName = pluginData.split("/")[0];
+
+            var commandLines = [];
+            for (var c = 0; c < plugin.commands.length; c++) {
+                var commandName = plugin.commands[c];
+
+                var command = plugin[commandName];
+                var commandText = "`" + config.prefix + commandName + "` - ";
+
+                if (command.needsAuth && !authorised) continue;
+                commandLines.push(commandText + (command.usage ? command.usage : "No usage defined.") + "\n");
+            }
+
+            if (commandLines.length) {
+                pluginName = pluginName.charAt(0).toUpperCase() + pluginName.slice(1);
+
+                result += "**"+ pluginName +"**\n";
+                result += commandLines.join("");
+                result += "\n";
+            }
+        }
+
+        var ownerResult = "";
+        if (message.author.id === config.ownerID) {
+            ownerResult = "**Owner**\n"+
+                          "`"+ config.prefix +"ping` - Checks the bot's latency.\n"+
+                          "`"+ config.prefix +"version` - Sends what version the bot is running.\n"+
+                          "`"+ config.prefix +"!stop` - Stops the bot.";
+        }
+
+        var embed = new Discord.RichEmbed()
+            .setColor(config.embedColor)
+            .setTitle(client.user.username +" Commands")
+            .setDescription("`"+ config.prefix +"help` - Sends the user a list of the available commands.\n\n"+ result + ownerResult)
+            .setThumbnail(client.user.avatarURL)
+            .setFooter("For additional help, contact TheV0rtex#4553")
+            .setTimestamp();
+
+        message.author.send({embed})
+            .then(() => { })
+            .catch(console.error);
+
+        message.reply("help has been sent.")
+            .then(m => m.delete(5000))
+            .catch(console.error);
     }
 
 });
